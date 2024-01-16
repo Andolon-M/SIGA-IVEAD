0fcf62c8526c8e82e8c1b2d0bbe64bbd39326eef
Andolon-M <aemendez-l@outlook.com>
2024-01-15 19:43:48 -0500

    nombramiento de version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -324,7 +324,7 @@
                     </ul>
                 </div>
                 <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+                <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Versión 0.0.1</a>. All Rights Reserved.</span>
             </div>
         </footer>
         <!--fin footer-->
